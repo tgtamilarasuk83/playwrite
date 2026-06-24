cfeaa401a76068475d0aa935d89a026edbd20df9
tgtamilarasuk83 <tgtamilarasuk83@gmail.com>
2026-06-24 16:59:20 +0530

    Update login.spec.ts

--- testpractice/login.spec.ts
+++ testpractice/login.spec.ts
@@ -5,7 +5,7 @@ test.describe('loginmodule', () => {
     console.log(process.env.BASE_URL!)
 
 
-//     await page.goto(process.env.BASE_URL!);
+    await page.goto(process.env.BASE_URL!);
 //     await page.fill('#username', process.env.USERNAME!);
 //     await page.fill('#password', process.env.PASSWORD!);
 //     await page.click('button[type="submit"]');
